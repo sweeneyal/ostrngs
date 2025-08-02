f2748196531f2514dd2c5c8e321129668619f999
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2025-08-02 02:48:57 -0500

    New tex files, new clock manager architecture to allow dynamic reconfig, and some extra documentation as a treat

--- hdl/rtl/TrngGenerator.vhd
+++ hdl/rtl/TrngGenerator.vhd
@@ -1,3 +1,6 @@
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+
 library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
@@ -29,7 +32,22 @@ entity TrngGenerator is
         -- entropy sample output 
         o_rng_data  : out std_logic_vector(31 downto 0);
         -- indicator that entropy sample is valid
-        o_rng_valid : out std_logic
+        o_rng_valid : out std_logic;
+
+        -- pll dynamic reconfiguration port address bus
+        i_pll_daddr : in std_logic_vector(6 downto 0);
+        -- pll dynamic reconfiguration port enable signal
+        i_pll_den   : in std_logic;
+        -- pll dynamic reconfiguration port write enable signal
+        i_pll_dwe   : in std_logic;
+        -- pll dynamic reconfiguration port write data bus
+        i_pll_di    : in std_logic_vector(15 downto 0);
+        -- pll dynamic reconfiguration port data ready signal
+        o_pll_drdy  : out std_logic;
+        -- pll dynamic reconfiguration port read data bus
+        o_pll_do    : out std_logic_vector(15 downto 0);
+        -- pll lock indicator
+        o_pll_locked : out std_logic
     );
 end entity TrngGenerator;
 
@@ -59,16 +77,47 @@ architecture rtl of TrngGenerator is
     );
 
     type rng_matrix_t is array (0 to cNumEntropySources - 1) of std_logic_vector(31 downto 0);
-
+    
     signal rng       : rng_matrix_t := (others => (others => '0'));
     signal rng_valid : std_logic_vector(cNumEntropySources - 1 downto 0) := (others => '0');
-
-    signal clk_mcx    : std_logic := '0';
-    signal resetn_mcx : std_logic := '0';
+    
+    constant cNumClocks : natural := 6;
+    type sel_matrix_t is array (0 to cNumEntropySources - 1) of std_logic_vector(cNumClocks - 2 downto 0);
+    signal clks       : std_logic_vector(cNumClocks - 1 downto 0) := (others => '0');
+    signal sel        : sel_matrix_t := (others => (others => '0'));
+    signal clk_sel    : std_logic_vector(cNumClocks - 2 downto 0) := (others => '0');
+    signal rng_clk    : std_logic := '0';
+    signal rng_resetn : std_logic := '0';
 begin
 
-    -- Add clock mux here
+    eClockManager : entity ostrngs.ClockManager
+    port map (
+        i_clk    => i_clk,
+        i_resetn => i_resetn,
+
+        i_pll_daddr => i_pll_daddr,
+        i_pll_den   => i_pll_den,
+        i_pll_dwe   => i_pll_dwe,
+        i_pll_di    => i_pll_di,
+        o_pll_drdy  => o_pll_drdy,
+        o_pll_do    => o_pll_do,
+
+        o_pll_clks   => clks,
+        o_pll_locked => o_pll_locked
+    );
+
+    clk_sel <= sel(to_integer(unsigned(i_rng_addr)));
+
+    eClockMux : entity ostrngs.ClockMux
+    generic map (
+        cNumClocks => cNumClocks
+    ) port map (
+        i_clks => clks,
+        i_sel  => clk_sel,
+        o_clk  => rng_clk
+    );
     
+    o_rng_clk   <= rng_clk;
     o_rng_data  <= rng(to_integer(unsigned(i_rng_addr)));
     o_rng_valid <= rng_valid(to_integer(unsigned(i_rng_addr)));
 
@@ -81,11 +130,15 @@ begin
 
             eMeshCoupledXor : entity ostrngs.MeshCoupledXor
             port map (
-                i_clk    => clk_mcx,
-                i_resetn => resetn_mcx,
+                i_clk    => rng_clk,
+                i_resetn => rng_resetn,
                 o_rng    => local_rng
             );
 
+            -- Makes a constant select value to be selected by the i_rng_addr bus to the mux.
+            sel(g_ii) <= std_logic_vector(to_unsigned(0, cNumClocks - 1));
+
+            -- Resize the random sample to fit a 32 bit word
             rng(g_ii) <= std_logic_vector(resize(unsigned(local_rng), 32));
 
             -- This entropy source generates a new random sample every clock cycle it is active.
@@ -102,3 +155,6 @@ begin
     end generate gEntropySourceInstantiation;
     
 end architecture rtl;
+
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
