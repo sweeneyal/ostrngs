194d935449996010ac9111c4fcb77c1a3a3ac877
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2025-08-26 23:31:52 -0500

    Fixed up the tcl script to actually work, rather than the garbage I wrote from memory previously

--- ostrngs.tcl
+++ ostrngs.tcl
@@ -3,7 +3,7 @@ namespace eval ::ostrngs {
     # Export commands
     namespace export help import_sources
 
-    set home [file tail [pwd]]
+    set home [pwd]
 }
 
 # Create a new stack
@@ -11,32 +11,49 @@ proc ::ostrngs::help {} {
     # Output an explanation of how this package works
 }
 
-proc ::ostrngs::import_sources args {
-    array set options { -mode project }
-    while {[llength $args]} {
-        switch -glob -- [lindex $args 0] {
-            -m*   {set args [lassign $args - options(-mode)]}
-            -*    {error "unknown option [lindex $args 0]"}
-            default break
+proc optproc {name args script} {
+    proc $name args [
+        string map [list ARGS $args SCRIPT $script] {
+            foreach var {ARGS} {
+                set [lindex $var 0] [lindex $var 1]
+            }
+
+            foreach {var val} $args {
+                set [string trim $var -] $val
+            }
+
+            SCRIPT
         }
-    }
-    puts "options: [array get options]"
+    ]
+}
 
+optproc ::ostrngs::import_sources {{mode "project"}} {
     # If we're running in project mode:
-    if { options(-mode) eq project } {
-        import_files [
-            $home/hdl/rtl/MeshCoupledXor/CxUnit.vhd
-            $home/hdl/rtl/MeshCoupledXor/MeshCoupledXor.vhd
+    if { $mode eq "project" } {
+        set files "
+            $ostrngs::home/hdl/rtl/TrngSandbox.vhd
+            $ostrngs::home/hdl/rtl/Common/AxiCrossbar.vhd
+            $ostrngs::home/hdl/rtl/Common/DualClockFifo.vhd
+            $ostrngs::home/hdl/rtl/Common/ClockMux.vhd
+            $ostrngs::home/hdl/rtl/Common/AxiUtility.vhd
+            $ostrngs::home/hdl/rtl/Common/DualClockBram.vhd
+            $ostrngs::home/hdl/rtl/Common/ClockManager.vhd
+            $ostrngs::home/hdl/rtl/OpenLoopMetaTrng/CoarseCascade.vhd
+            $ostrngs::home/hdl/rtl/OpenLoopMetaTrng/OpenLoopMetaTrng.vhd
+            $ostrngs::home/hdl/rtl/TrngTestbedCore.vhd
+            $ostrngs::home/hdl/rtl/StrTrng/StrTrng.vhd
+            $ostrngs::home/hdl/rtl/StrTrng/MullerC.vhd
+            $ostrngs::home/hdl/rtl/StrTrng/SelfTimedRing.vhd
+            $ostrngs::home/hdl/rtl/TrngTestbed.vhd
+            $ostrngs::home/hdl/rtl/TrngGenerator.vhd
+            $ostrngs::home/hdl/rtl/TrngStorage.vhd
+            $ostrngs::home/hdl/rtl/MeshCoupledXor/CxUnit.vhd
+            $ostrngs::home/hdl/rtl/MeshCoupledXor/MeshCoupledXor.vhd
+        "
+        import_files $files
 
-            $home/hdl/rtl/OpenLoopMetaTrng/CoarseCascade.vhd
-            $home/hdl/rtl/OpenLoopMetaTrng/OpenLoopMetaTrng.vhd
-
-            $home/hdl/rtl/StrTrng/MullerC.vhd
-            $home/hdl/rtl/StrTrng/SelfTimedRing.vhd
-            $home/hdl/rtl/StrTrng/StrTrng.vhd
-
-            $home/hdl/rtl/TrngGenerator.vhd
-            $home/hdl/rtl/TrngTestbed.vhd
-        ]
+        foreach file $files {
+            set_property library ostrngs [get_files [file tail $file]]
+        }
     }
 }
